bff96b6fe20e23cec31e0f858b88bdd439b65d8f
Andriy Kislitsyn <andriykislitsyn@gmail.com>
2020-09-29 09:55:09 +0300

    Refactor Safari

--- macuitest/lib/apps/browser.py
+++ macuitest/lib/apps/browser.py
@@ -1,142 +1,92 @@
 import glob
-import logging
 import os
 import time
-from typing import List
+from typing import ClassVar, List
 
-from macuitest.lib.applescript_lib.applescript_wrapper import as_wrapper, AppleScriptError
+from macuitest.config.constants import MINUTE
+from macuitest.lib.applescript_lib.applescript_wrapper import as_wrapper
 from macuitest.lib.apps.application import Application
 from macuitest.lib.core import wait_condition
 from macuitest.lib.elements.native.native_ui_element import NativeUIElement
 from macuitest.lib.operating_system.env import env
-from macuitest.lib.operating_system.macos import macos
 
 
-class DownloadHasNotStartedException(Exception):
-    pass
-
-
-class DownloadHasNotFinishedException(Exception):
-    pass
-
-
-class MaliciousFileWarning(Exception):
-    """Thrown when Chrome shows a warning after downloading a malicious file."""
-    pass
-
-
-class _BaseBrowser(Application):
-    def __init__(self, app_name):
-        super().__init__(app_name)
-        self.browser_name = app_name
-        self.document_type = None
-        self.download_temp_pattern = None
-        self.downloads_count = -1
-
-    def close_tabs(self):
-        try:
-            as_wrapper.execute(f'tell every window of application "{self.name}" to close tabs')
-            wait_condition(lambda: not self._get_tabs_number())
-        except AppleScriptError:
-            pass
-
-    def _get_tabs_number(self) -> List[List[str]]:
-        try:
-            return as_wrapper.execute(f'tell application "{self.name}" to get name of every tab of every window')
-        except AppleScriptError:
-            pass
-
-    def download_file(self, url, file_name='file_pattern*', expected_file_size=2 * 10 ** 5):
-        self.launch()
-        self.open_link_as_user(url, is_download_confirmation_needed=True)
-        self.wait_download_finished(env.downloads)
-        self.close_tabs()
-        file_ = wait_condition(glob.glob, 30, os.path.join(env.downloads, file_name))
-        assert file_, f'File missing. Contents of `Downloads` folder: {os.listdir(env.downloads)}'
-        logging.info(f'File: {file_[0]}')
-        file_size = os.path.getsize(file_[0])
-        logging.info(f'Size: {file_size}')
-        assert file_size >= expected_file_size, f'Package size < 200KB. Actual size is: {file_size / 10 ** 3}KB'
-        return file_[0]
-
-    @staticmethod
-    def open_link_in_a_new_tab(url):
-        as_wrapper.send_keystroke('t', 'command')
-        time.sleep(1)
-        as_wrapper.typewrite(url)
-        time.sleep(1)
-        as_wrapper.send_keycode(as_wrapper.key_codes['return'])
-        time.sleep(.5)
-
-    def open_link_as_user(self, url, is_download_confirmation_needed=False):
-        self.open_new_tab()
-        self.set_address_bar_value(url)
-        as_wrapper.send_keycode(as_wrapper.key_codes['return'])
-        if is_download_confirmation_needed:
-            self.downloads_count = self.files_in_downloads_dir
-            self.confirm_download()
-
-    def open_new_tab(self):
-        as_wrapper.tell_app(self.name, f'make new {self.document_type}')
-        time.sleep(1)
-
-    def open_urls(self, urls: List[str]):
-        for url in urls:
-            self.open_link_as_user(url)
-
-    def set_address_bar_value(self, new_value):
-        macos.shell_executor.execute(f'printf "{new_value}" |pbcopy')
-        as_wrapper.send_keystroke('v', 'command')  # Paste the data
-        time.sleep(1)
-
-    def confirm_download(self):
-        pass
-
-    def wait_download_finished(self, target_directory, timeout=180):
-        """Wait for a temporary file to be removed from the download directory."""
-        time.sleep(3)
-        if not wait_condition(lambda: self.files_in_downloads_dir > self.downloads_count, 120):
-            raise DownloadHasNotStartedException  # Sometimes resolver works slower than we need. So we wait 2m.
-        if not wait_condition(lambda: glob.glob(os.path.join(target_directory, self.download_temp_pattern)) == [],
-                              timeout):
-            raise DownloadHasNotFinishedException
-
-    def webpage_did_load(self, webpage_address):
-        assert wait_condition(lambda: isinstance(self.active_tab_url, str), timeout=30)
-        return wait_condition(lambda: webpage_address in self.active_tab_url, timeout=30)
-
-    @property
-    def active_tab_url(self):
-        return wait_condition(lambda: as_wrapper.execute(self._get_url_command), timeout=30)
-
-    @property
-    def files_in_downloads_dir(self):
-        return len([file_ for file_ in os.listdir(env.downloads) if not file_.startswith('.')])
-
-    @property
-    def _get_url_command(self):
-        return f'''
-            try
-                tell application "{self.name}" to set page_address to URL of last tab of window 1
-                    if page_address as string = "missing value" then
-                        return false
-                    end if
-                return page_address as string
-            end try'''
-
-
-class _Safari(_BaseBrowser):
+class Safari(Application):
     def __init__(self, app_name='Safari'):
         super().__init__(app_name)
-        self.document_type = 'document'
-        self.download_temp_pattern = '*.download'
+        self.bundle_id: ClassVar[str] = 'com.apple.Safari'
 
-    def set_address_bar_value(self, new_value: str):
-        self.window.wait_displayed(timeout=10)
-        address_bar = NativeUIElement.from_bundle_id('com.apple.Safari').windows()[0].find_element(
-            AXIdentifier='WEB_BROWSER_ADDRESS_AND_SEARCH_FIELD', recursive=True)
+    def download_file(self, url: str, file_name: str = '*pattern*', timeout: int = MINUTE):
+        _before_ = self.downloads
+        self.request_webpage(url)
+        self.confirm_download()
+        assert wait_condition(lambda: self.downloads > _before_, timeout=MINUTE)
+        assert wait_condition(lambda: glob.glob(os.path.join(env.downloads, '*.download')) == [], timeout=timeout)
+        return wait_condition(glob.glob, 30, os.path.join(env.downloads, file_name))
+
+    def request_webpage(self, url: str):
+        as_wrapper.tell_app(self.name, 'make new document with properties {URL:"%s"}' % url)
+
+    def close_tabs(self):
+        if self.native_window:
+            as_wrapper.tell_app(self.name, 'close tabs of front window')
+            wait_condition(lambda: not self.tabs)
+
+    def did_webpage_load(self, webpage_address: str) -> bool:
+        wait_condition(lambda: self.stop_reload_button is not None)
+        wait_condition(lambda: self.stop_reload_button.AXTitle == 'Reload this page')
+        wait_condition(lambda: self.execute_js_command('document.readyState') == 'complete')
+        return webpage_address in self.address_bar_value
+
+    def execute_js_command(self, command: str) -> None:
+        return as_wrapper.tell_app(self.name, f'''tell front document to do JavaScript "{command}"''')
+
+    def confirm_download(self) -> None:
+        if confirm_download_dialog := self.confirm_download_dialog:
+            confirm_download_dialog.find_element(AXRole='AXButton', AXTitle='Allow').AXPress()
+
+    def __get_address_bar_value(self) -> str:
+        return self.address_bar.AXValue
+
+    def __set_address_bar_value(self, new_value: str) -> None:
+        address_bar = self.address_bar
+        address_bar.AXFocused = True
+        time.sleep(.5)
         address_bar.AXValue = new_value
-        time.sleep(1)
+
+    address_bar_value = property(__get_address_bar_value, __set_address_bar_value)
+
+    @property
+    def stop_reload_button(self) -> NativeUIElement:
+        return self.address_bar.AXParent.find_element(AXIdentifier='StopReloadButton')
+
+    @property
+    def address_bar(self) -> NativeUIElement:
+        return self.tool_bar.find_element(AXIdentifier='WEB_BROWSER_ADDRESS_AND_SEARCH_FIELD', recursive=True)
+
+    @property
+    def tool_bar(self) -> NativeUIElement:
+        return self.native_window.find_element(AXRole='AXToolbar')
+
+    @property
+    def tabs(self) -> List[NativeUIElement]:
+        try:
+            result = self.native_window.find_element(AXIdentifier='TabBar').AXChildren
+        except AttributeError:
+            result = []
+        return result
+
+    @property
+    def confirm_download_dialog(self) -> NativeUIElement:
+        return self.native_window.find_element(AXIdentifier='AXSafariModalDialog', recursive=True)
+
+    @property
+    def native_window(self) -> NativeUIElement:
+        return NativeUIElement.from_bundle_id(self.bundle_id).find_element(AXRole='AXWindow', AXMain=True)
+
+    @property
+    def downloads(self) -> List[str]:
+        return [f for f in os.listdir(env.downloads) if not f.startswith('.')]
 
 
-safari = _Safari()
+safari = Safari()
--- macuitest/lib/elements/applescript_element.py
+++ macuitest/lib/elements/applescript_element.py
@@ -40,7 +40,7 @@ class BaseUIElement:
         monitor.save_screenshot(region=_region, where=_where)
         return _where
 
-    def region(self, margin: int = 4):
+    def region(self, margin: int = 0):
         self.reset_frame()
         return Region(self.frame.x1 - margin, self.frame.y1 - margin, self.frame.x2 + margin, self.frame.y2 + margin)
 
