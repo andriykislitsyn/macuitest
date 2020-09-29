fa0544f8fc69e4e244c14368e6afe17ed9bfcff1
Andriy Kislitsyn <andriykislitsyn@gmail.com>
2020-09-29 18:35:14 +0300

    Increment version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('README.md') as fh:
 setup(
     name='macuitest',
     packages=find_packages(),
-    version='0.7.15',
+    version='0.7.16',
     license='Apache-2.0 License',
     description='A simple UI testing framework for macOS',
     long_description=long_description,
@@ -19,7 +19,7 @@ setup(
     author='Andrii Kislitsyn',
     author_email='andriikislitsyn@gmail.com',
     url='https://github.com/andriykislitsyn',
-    download_url='https://github.com/andriykislitsyn/macuitest/archive/v0.7.15-alpha.tar.gz',
+    download_url='https://github.com/andriykislitsyn/macuitest/archive/v0.7.16-alpha.tar.gz',
     keywords=['Testing', 'UI', 'Functional', 'macOS'],
     install_requires=[
         'biplist',
